3d99e0d8f5a20b420404f6bc38c6fcb9024bf132
Yury Tsybizov <yuriy.tsibizov@gmail.com>
2020-10-23 13:57:57 +0200

    ensure that bluetooth and UART are started, make scripts owned by root

--- stage3/02-install-midibox/01-run.sh
+++ stage3/02-install-midibox/01-run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -e
 
+rm -rf midibox
 git clone https://github.com/ytsibizov/midibox
 
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/opt/midibox"
-install -v -o 1000 -g 1000 -m 644 "midibox/src/midibox.py" "${ROOTFS_DIR}/opt/midibox/"
-install -v -o 1000 -g 1000 -m 755 "midibox/service/midibox.sh" "${ROOTFS_DIR}/opt/midibox/"
+install -v -o 0 -g 0 -d "${ROOTFS_DIR}/opt/midibox"
+install -v -o 0 -g 0 -m 644 "midibox/src/midibox.py" "${ROOTFS_DIR}/opt/midibox/"
+install -v -o 0 -g 0 -m 755 "midibox/service/midibox.sh" "${ROOTFS_DIR}/opt/midibox/"
 install -v -o 0 -g 0 -m 644 "midibox/service/midibox.service" "${ROOTFS_DIR}/etc/systemd/system/"
 
 echo "Enable midibox service"
 
 on_chroot << EOF
 systemctl daemon-reload
+systemctl enable bluetooth
+systemctl enable hciuart
 systemctl enable midibox
 EOF
 
